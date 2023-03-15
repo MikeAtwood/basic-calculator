403a02917550c6d5025e96f830f5ef30be3a0f3b
Deadpool1 <atwood169@yahoo.com>
2023-03-15 17:25:41 -0500

    created comp in js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+const numberBtn = document.querySelectorAll(".number")
+const clearBtn = document.querySelector(".clear")
+const equalBtn = document.querySelector(".equal")
+const decimalBtn = document.querySelector(".decimal")
+const operatorBtn = document.querySelectorAll(".operator")
+const prevBtn = document.querySelector(".previous")
+const curBtn = document.querySelector(".current")
+
 const add = function(a,b) {
     return a + b;
 }
@@ -15,5 +27,6 @@ const divide = function(a,b) {
 }
 
 const operate = () => {
-    
+
 }
+
