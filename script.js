463678f59af78bbd4faa789763292dd7ad394d76
Deadpool1 <atwood169@yahoo.com>
2023-03-15 17:42:39 -0500

    added eventlisteners

--- script.js
+++ script.js
@@ -7,8 +7,30 @@ const clearBtn = document.querySelector(".clear")
 const equalBtn = document.querySelector(".equal")
 const decimalBtn = document.querySelector(".decimal")
 const operatorBtn = document.querySelectorAll(".operator")
-const prevBtn = document.querySelector(".previous")
-const curBtn = document.querySelector(".current")
+const previousScreen = document.querySelector(".previous")
+const currentScreen = document.querySelector(".current")
+
+// Event Handlers
+numberBtn.forEach((number) => number.addEventListener("click", function(e) {
+    handleNumber(e.target.textContent)
+    currentScreen.textContent = currentValue;
+}))
+
+operatorBtn.forEach((op) => op.addEventListener("click", function(e) {
+    handleOperator(e.target.textContent)
+}))
+
+
+function handleNumber(num) {
+    if(currentValue.length <= 11) {
+    currentValue += num;
+    }
+}
+
+function handleOperator(op) {
+    console.log(op)
+}
+
 
 const add = function(a,b) {
     return a + b;
